6ae391d5659448d8d7303812fc62d048932de53c
王楠 <wangn0729@163.com>
2020-03-04 10:59:34 +0800

    first commit

---
+++ MainAppB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainAppB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is MainB Application");
+
+            Console.ReadLine();
+
+        }
+    }
+}
